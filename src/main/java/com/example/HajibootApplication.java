ba75c9fbad365dc3b2cd9d314c084ed97cc23558
‘shupo’ <hiroshimashuhei@me.com>
2017-11-10 14:41:26 +0900

    working on p39

--- src/main/java/com/example/HajibootApplication.java
+++ src/main/java/com/example/HajibootApplication.java
@@ -1,21 +1,19 @@
 package com.example;
 
+import com.example.app.Frontend;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@RestController
 public class HajibootApplication {
 
-	@GetMapping
-	String home() {
-		return "Hello World!";
-	}
-
 	public static void main(String[] args) {
-		SpringApplication.run(HajibootApplication.class, args);
+		ApplicationContext context = SpringApplication.run(HajibootApplication.class, args);
+		Frontend frontend = context.getBean(Frontend.class);
+		frontend.run();
 	}
 }
-
